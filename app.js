2af96b3ab6fb5e42613ef78c5441ca60c1da5c93
Samantha Spensley <spenslsm@gmail.com>
2016-10-15 12:17:55 -0600

    new post form finally working

---
+++ app.js
@@ -0,0 +1,68 @@
+var app = angular.module("reddit", []);
+
+
+app.controller('MainController', function($scope){
+  $scope.view = {};
+  $scope.view.showPostForm = false;
+  $scope.view.sort = 'votes';
+  $scope.view.posts = [
+    {
+      title: "Black Canyon of the Gunnison",
+      author: "Samantha Spensley",
+      image: "https://www.nps.gov/common/uploads/grid_builder/imr/crop16_9/41DEC438-1DD8-B71B-0BB27A016E404D9F.jpg?width=465&quality=90&mode=crop",
+      description: "Big enough to be overwhelming, still intimate enough to feel the pulse of time, Black Canyon of the Gunnison exposes you to some of the steepest cliffs, oldest rock, and craggiest spires in North America. With two million years to work, the Gunnison River, along with the forces of weathering, has sculpted this vertical wilderness of rock, water, and sky.",
+      votes: 4,
+      comments: []
+    },
+    {
+      title: "Colorado National Monument",
+      author: "Samantha Spensley",
+      image: "https://cdn-co.milespartnership.com/sites/default/master/files/coloradonationalmonument_MI.jpg",
+      description: "Colorado National Monument preserves one of the grand landscapes of the American West. But this treasure is much more than a monument. Towering monoliths exist within a vast plateau and canyon panorama. You can experience sheer-walled, red rock canyons along the twists and turns of Rim Rock Drive, where you may spy bighorn sheep and soaring eagles.",
+      votes: 2,
+      comments: []
+    },
+    {
+      title: "Great Sand Dunes",
+      author: "Samantha Spensley",
+      image: "https://www.nps.gov/common/uploads/grid_builder/imr/crop16_9/8EADDB13-1DD8-B71B-0B43FFA9BE6A3D82.jpg?width=950&quality=90&mode=crop",
+      description: "The tallest dunes in North America are the centerpiece in a diverse landscape of grasslands, wetlands, conifer and aspen forests, alpine lakes, and tundra. Experience this diversity through hiking, sand sledding, splashing in Medano Creek, wildlife watching, and more!",
+      votes: 3,
+      comments: []
+    },
+  ];
+
+  // functions
+  $scope.submitPost = function(form){
+    $scope.view.posts.push({
+      title: $scope.view.title,
+      author: $scope.view.author,
+      image: $scope.view.image,
+      description: $scope.view.description,
+      votes: 0,
+      date: Date.now(),
+      comments: [],
+      showComments: false,
+      showCommentForm: false
+    });
+    $scope.togglePostForm();
+    form.$setPristine();
+    $scope.view.title = '';
+    $scope.view.author = '';
+    $scope.view.image = '';
+    $scope.view.description = '';
+  };
+
+  $scope.togglePostForm = function() {
+    $scope.view.showPostForm = !$scope.view.showPostForm
+  }
+
+  $scope.addVote = function(index) {
+    $scope.view.post[index].votes++;
+  }
+
+  $scope.removeVote = function(index) {
+    $scope.view.post[index].votes--;
+  }
+
+});
